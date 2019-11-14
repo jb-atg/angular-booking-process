b047179f446e1f322957c28bf3101d02239f4375
jb-atg <justin.beattie@aptouring.com.au>
2019-11-14 17:57:51 +1100

    3 files added, 5 deleted

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { StepDeparturesComponent } from './step-departures/step-departures.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AppComponent, StepDeparturesComponent]
+})
+export class BookingProcessModule { }
---
+++ app/step-departures/step-departures.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-step-departures',
+  templateUrl: './step-departures.component.html',
+  styleUrls: ['./step-departures.component.scss']
+})
+export class StepDeparturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
