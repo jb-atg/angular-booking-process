d452804b69ebc86de50dc1be66d04b792845b38d
jb-atg <justin.beattie@aptouring.com.au>
2019-11-14 17:41:27 +1100

    1 file modified, 5 added

---
+++ app/booking-process/booking-process.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookingProcessComponent } from './booking-process/booking-process.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BookingProcessComponent]
+})
+export class BookingProcessModule { }
